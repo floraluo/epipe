edf8a1927832972f352dbee2e4c737f76d15e097
floraluo <lf_radish@163.com>
2016-01-11 17:57:03 +0800

    add addtional color

--- www/js/supplier/controller.js
+++ www/js/supplier/controller.js
@@ -310,8 +310,8 @@ angular.module('starter.controller' , [])
 	}])
 
 	// 订单列表
-	.controller('orderListCtrl', ['$scope','httpService', 'order', '$state', '$stateParams', '$ionicPopup','$http',
-		function($scope, httpService, order, $state, $stateParams,$ionicPopup, $http){
+	.controller('orderListCtrl', ['$scope','httpService', '$state', '$stateParams', '$ionicPopup','$http',
+		function($scope, httpService, $state, $stateParams,$ionicPopup, $http){
 		$scope.order = {
 			canBeLoaded: false,
 			content: [],
@@ -378,99 +378,94 @@ angular.module('starter.controller' , [])
 			// }
 		}
 	}])
-// 报价
-.controller('quotationCtrl', ['$scope', '$ionicPopup', 'httpService', '$state', '$stateParams','$ionicHistory', '$ionicTabsDelegate', 'order', 
-	function($scope, $ionicPopup, httpService, $state, $stateParams, $ionicHistory, $ionicTabsDelegate, order){
-
-	// 隐藏tabs
-	$ionicTabsDelegate.showBar(false);
-
-	// show_error：是否显示错误提示
-	$scope.show_error = false;
-	$scope.quo = {
-		btnText: "报价",
-		isFirst: false
-	}
-	$scope.productQuo = {
-		unitPrice: '',
-		totalPrice: ''
-	}
-	// 订单信息
-	httpService.get('/order/getByOrderName/'+$stateParams.orderId)
-	.success(function(data){
-		$scope.order = data.data;
-	})
-	// 报价记录
-	httpService.get('/order/getSupQuotation/'+$stateParams.orderId)
-	.success(function(data){
-		if(data.status){
-			var quotation = data.data;
-			$scope.productQuo = {
-				unitPrice: quotation.unitPrice,
-				totalPrice: quotation.sumPrice
-			}
-			$scope.quo.btnText = "修改报价";
-		}else {
-			$scope.quo.isFirst = true;
-		}
-	})
-
-	// 计算总价
-	$scope.canlTotalPrice = function(orderNum){
-		$scope.productQuo.unitPrice = parseInt($scope.productQuo.unitPrice) > 0 ? $scope.productQuo.unitPrice : '';
-		$scope.productQuo.totalPrice = $scope.productQuo.unitPrice*orderNum !=0 ? $scope.productQuo.unitPrice*orderNum : ''
-	}
-
 	// 报价
-	$scope.quotation = function(myform){
-		if(myform.$dirty){
-			$scope.show_error = true;
-			if(myform.$valid){
-				httpService.post("/order/setQuotation", {
-					unitPrice: $scope.productQuo.unitPrice,
-					sumPrice: $scope.productQuo.totalPrice,
-					orderName: $stateParams.orderId
-				})
-				.success(function(data){
-					if(data.status){
-						var promise = $ionicPopup.alert({
-							template: "报价完成，返回订单列表",
-							okText: "确认"
-						});
-						promise.then(function(data){
-							if(data){
-								$state.go("supplier.orderList");								
-							}
-						})
-					}
-				})
-				// $state.go("supplier.orderDetail",{orderId:$stateParams.orderId})
+	.controller('quotationCtrl', ['$scope', '$ionicPopup', 'httpService', '$state', '$stateParams','$ionicHistory', '$ionicTabsDelegate', 
+		function($scope, $ionicPopup, httpService, $state, $stateParams, $ionicHistory, $ionicTabsDelegate){
+
+		// 隐藏tabs
+		$ionicTabsDelegate.showBar(false);
+
+		// show_error：是否显示错误提示
+		$scope.show_error = false;
+		$scope.quo = {
+			btnText: "报价",
+			isFirst: false
+		}
+		$scope.productQuo = {
+			unitPrice: '',
+			totalPrice: ''
+		}
+		// 订单信息
+		httpService.get('/order/getByOrderName/'+$stateParams.orderId)
+		.success(function(data){
+			$scope.order = data.data;
+		})
+		// 报价记录
+		httpService.get('/order/getSupQuotation/'+$stateParams.orderId)
+		.success(function(data){
+			if(data.status){
+				var quotation = data.data;
+				$scope.productQuo = {
+					unitPrice: quotation.unitPrice,
+					totalPrice: quotation.sumPrice
+				}
+				$scope.quo.btnText = "修改报价";
+			}else {
+				$scope.quo.isFirst = true;
+			}
+		})
+
+		// 计算总价
+		$scope.canlTotalPrice = function(orderNum){
+			$scope.productQuo.unitPrice = parseInt($scope.productQuo.unitPrice) > 0 ? $scope.productQuo.unitPrice : '';
+			$scope.productQuo.totalPrice = $scope.productQuo.unitPrice*orderNum !=0 ? $scope.productQuo.unitPrice*orderNum : ''
+		}
+
+		// 报价
+		$scope.quotation = function(myform){
+			if(myform.$dirty){
+				$scope.show_error = true;
+				if(myform.$valid){
+					httpService.post("/order/setQuotation", {
+						unitPrice: $scope.productQuo.unitPrice,
+						sumPrice: $scope.productQuo.totalPrice,
+						orderName: $stateParams.orderId
+					})
+					.success(function(data){
+						if(data.status){
+							var promise = $ionicPopup.alert({
+								template: "报价完成，返回订单列表",
+								okText: "确认"
+							});
+							promise.then(function(data){
+								if(data){
+									$state.go("supplier.orderList");								
+								}
+							})
+						}
+					})
+				}
 			}
 		}
-	}
-}])
-// 订单详情
-.controller('orderDetailCtrl', ['$scope','$http', '$state','$stateParams', '$ionicHistory','$location', '$ionicTabsDelegate', 'order', 'supplier', 
-		function($scope, $http,$state, $stateParams, $ionicHistory, $location, $ionicTabsDelegate, order, supplier){
-	// 隐藏tabs
-	$ionicTabsDelegate.showBar(false);
-	// $scope.order = order[$stateParams.orderId]
-	// $ionicHistory.goBack(-1);
-	// $scope.$on("$ionicView.enter", function () {
-	//    $ionicHistory.clearCache();
-	//    $ionicHistory.clearHistory();
-	// });
-	$http.get('../data/orderDetail'+$stateParams.orderId+'.json')
-	.success(function(data){
-		$scope.order = data;
-	})
-	var promise = supplier.getSupplierInfo();
-	promise.then(function(data){
-		$scope.supplier = data;
-	}, function(data){
-		// $scope.supplier = 
-	})
-}])
+	}])
+	// 订单详情
+	.controller('orderDetailCtrl', ['$scope','httpService','$stateParams', '$ionicHistory', '$ionicTabsDelegate', 
+			function($scope, httpService, $stateParams, $ionicHistory, $ionicTabsDelegate){
+		// 隐藏tabs
+		$ionicTabsDelegate.showBar(false);
+		// $scope.order = order[$stateParams.orderId]
+		// $ionicHistory.goBack(-1);
+		// $scope.$on("$ionicView.enter", function () {
+		//    $ionicHistory.clearCache();
+		//    $ionicHistory.clearHistory();
+		// });
+
+		// 订单信息
+		httpService.get('/order/getByOrderName/'+$stateParams.orderId)
+		.success(function(data){
+			$scope.order = data.data;
+		})
+	}])
 // 物流跟踪
 .controller('LogisTrackCtrl', ['$scope','$http','$stateParams', '$ionicTabsDelegate', function($scope, $http,$stateParams, $ionicTabsDelegate){
 	// 隐藏tabs
--- www/js/supplier/service.js
+++ www/js/supplier/service.js
@@ -1,10 +1,6 @@
 angular.module('starter.service', [])
-    // .factory('orderList',['$q', '$http', function($q, $http){
-
-// }])
 .factory('httpService', ['$q', '$http', 'CONFIG', '$cookieStore','$ionicBackdrop', '$ionicLoading',
     function($q, $http,  CONFIG, $cookieStore,$ionicBackdrop, $ionicLoading) {
-
         return {
             post: function(url, params) {
                 // body...
@@ -61,17 +57,17 @@ angular.module('starter.service', [])
                     $ionicBackdrop.release();
                     $ionicLoading.hide();
                 });
-
             }
-
-
-
         }
-
-
     }
 ])
-
+// .factory('order',['$q', '$http', 'httpService', function($q, $http, httpService){
+//     return {
+//         getByOrderName: function(orderId){  
+//             return httpService.get('/order/getByOrderName/'+ orderId)
+//         }
+//     }
+// }])
 .factory('supplier', ['$q', '$http', function($q, $http) {
         // interface
         var supplier = {
@@ -95,63 +91,63 @@ angular.module('starter.service', [])
             return def.promise;
         }
     }])
-    .factory('order', ['$q', '$http', function($q, $http) {
-        var order = {
-            info: [],
-            getOrder: getOrder
-        }
-        return order;
+    // .factory('order', ['$q', '$http', function($q, $http) {
+    //     var order = {
+    //         info: [],
+    //         getOrder: getOrder
+    //     }
+    //     return order;
 
-        function getOrder() {
-            var def = $q.defer();
+    //     function getOrder() {
+    //         var def = $q.defer();
 
-            $http.get("../data/order.json")
-                .success(function(data) {
-                    order.info = data;
-                    def.resolve(data);
-                })
-                .error(function() {
-                    def.reject("Failed to get order info")
-                });
-            return def.promise;
-        }
-        // var order = [
-        // 	{
-        // 		"orderId": "0",
-        // 		"orderNum": "3736837484",
-        // 		"productName": "高密度聚乙烯PPD沙发客3023",
-        // 		"productAmount": "4",
-        // 		"date": "2015-02-05",
-        // 		"state": "我要报价",
-        // 		"trackInfo": false,
-        // 		"purchaserCompany": "浙江金鱼枫叶管理有限公司",
-        // 		"purchaserTel": "12354896578",
+    //         $http.get("../data/order.json")
+    //             .success(function(data) {
+    //                 order.info = data;
+    //                 def.resolve(data);
+    //             })
+    //             .error(function() {
+    //                 def.reject("Failed to get order info")
+    //             });
+    //         return def.promise;
+    //     }
+    //     // var order = [
+    //     // 	{
+    //     // 		"orderId": "0",
+    //     // 		"orderNum": "3736837484",
+    //     // 		"productName": "高密度聚乙烯PPD沙发客3023",
+    //     // 		"productAmount": "4",
+    //     // 		"date": "2015-02-05",
+    //     // 		"state": "我要报价",
+    //     // 		"trackInfo": false,
+    //     // 		"purchaserCompany": "浙江金鱼枫叶管理有限公司",
+    //     // 		"purchaserTel": "12354896578",
 
-        // 		"unitPrice": "3456",
-        // 		"totalPrice": "637383746"
-        // 	},
-        // 	{
-        // 		"orderId": "1",
-        // 		"orderNum": "3453453452334",
-        // 		"productName": "高密度3",
-        // 		"productAmount": "0.9",
-        // 		"date": "2015-02-05",
-        // 		"state": "已发货",
-        // 		"trackInfo": true,
-        // 		"purchaserCompany": "浙江金鱼枫叶管理有限公司",
-        // 		"purchaserTel": "1254896587"
-        // 	},
-        // 	{
-        // 		"orderId": "2",
-        // 		"orderNum": "23423424234",
-        // 		"productName": "高密度聚乙烯PPD沙发客3023",
-        // 		"productAmount": "22",
-        // 		"date": "2014-23-05",
-        // 		"state": "已完成",
-        // 		"trackInfo": false,
-        // 		"purchaserCompany": "浙江金鱼枫叶管理有限公司",
-        // 		"purchaserTel": "34234252354"
-        // 	}
-        // ];
-        // return order;
-    }])
+    //     // 		"unitPrice": "3456",
+    //     // 		"totalPrice": "637383746"
+    //     // 	},
+    //     // 	{
+    //     // 		"orderId": "1",
+    //     // 		"orderNum": "3453453452334",
+    //     // 		"productName": "高密度3",
+    //     // 		"productAmount": "0.9",
+    //     // 		"date": "2015-02-05",
+    //     // 		"state": "已发货",
+    //     // 		"trackInfo": true,
+    //     // 		"purchaserCompany": "浙江金鱼枫叶管理有限公司",
+    //     // 		"purchaserTel": "1254896587"
+    //     // 	},
+    //     // 	{
+    //     // 		"orderId": "2",
+    //     // 		"orderNum": "23423424234",
+    //     // 		"productName": "高密度聚乙烯PPD沙发客3023",
+    //     // 		"productAmount": "22",
+    //     // 		"date": "2014-23-05",
+    //     // 		"state": "已完成",
+    //     // 		"trackInfo": false,
+    //     // 		"purchaserCompany": "浙江金鱼枫叶管理有限公司",
+    //     // 		"purchaserTel": "34234252354"
+    //     // 	}
+    //     // ];
+    //     // return order;
+    // }])
